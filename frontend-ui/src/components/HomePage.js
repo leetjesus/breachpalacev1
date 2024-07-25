af8b7ce2325a117d3c8e1f9a78ae4a069fedc41a
leetjesus <JesusSecc@protonmail.com>
2024-07-25 16:01:04 -0700

    Home page

---
+++ frontend-ui/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopBar from './topbar';
+import Container from './container';
+import Background from './background';
+
+function HomePage() {
+  return (
+    <div>
+      <TopBar />
+      <Container />
+      <Background />
+    </div>
+  );
+}
+
+export default HomePage;
