1e5abc5ba2a63879a4a2ab237cc2968f7ec6d579
leetjesus <JesusSecc@protonmail.com>
2024-07-31 15:17:35 -0700

    Added result page to router

--- frontend-ui/src/App.js
+++ frontend-ui/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import HomePage from './components/HomePage';
 import ContactPage from './components/contactPage';
 import AboutPage from './components/aboutPage'
+import ResultPage from './components/resultPage'
 import { Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </div>
   );
