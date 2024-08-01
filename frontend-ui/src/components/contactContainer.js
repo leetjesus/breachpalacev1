5dc96bf9a3d08fcc4364b10fe32684fcefa217e8
leetjesus <JesusSecc@protonmail.com>
2024-08-01 15:40:28 -0700

    added spacing between input

--- frontend-ui/src/components/contactContainer.js
+++ frontend-ui/src/components/contactContainer.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './contactform.css'; // Assuming you will use an external CSS file
 import DocumentTitle from './DocumentTitle';
 
+import { Button} from "react-bootstrap";
+
 const ContactForm = () => {
   DocumentTitle("Contact BreachPalace")
   const [charCount, setCharCount] = useState(0);
@@ -15,26 +17,29 @@ const ContactForm = () => {
       <h1>Contact Us</h1>
       <form action="/submit_contact_form/" method="post">
         <div className="form-group">
-          <input type="text" id="name" name="name" className="form-control" placeholder="Your Name" />
+          <input type="text" id="name" name="name" className="form-control" placeholder="Name" />
         </div>
+        <br></br>
         <div className="form-group">
-          <input type="email" id="email" name="email" className="form-control" placeholder="Your Email" />
+          <input type="email" id="email" name="email" className="form-control" placeholder="Email" />
         </div>
+        <br></br>
         <div className="form-group">
           <textarea 
             id="message" 
             name="message" 
             className="form-control" 
             rows="5" 
-            placeholder="Your Message" 
+            placeholder="Message" 
             maxLength="1000" 
             onChange={handleChange}
           ></textarea>
+          <br></br>
           <div className="char-counter">
             <span>{charCount}</span>/1000
           </div>
         </div>
-        <button type="submit" className="btn-custom">Send Message</button>
+        <Button type="submit" variant="light">Send Message</Button>
       </form>
     </div>
   );
