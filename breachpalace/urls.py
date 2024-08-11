bbfcb876b5f069afd479d911f30f50fa1a929f95
leetjesus <JesusSecc@protonmail.com>
2024-08-10 18:42:25 -0700

    url end-point removed from master dir

--- breachpalace/urls.py
+++ breachpalace/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
+# "Ohhhh that is sick!" left off here
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend_api.urls')),
+    path('', include('backend_api.urls')),
+    path('', include('backend_api.urls')),
     # React HTML template
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
