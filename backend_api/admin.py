a1f8e319076e51c326db3bf4ce9edc4f488dfb85
leetjesus <JesusSecc@protonmail.com>
2024-08-10 18:40:46 -0700

    added contactformAdmin and fieldsets

--- backend_api/admin.py
+++ backend_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import leakdata
 from .models import emailList
+from .models import contactForm
 
 # Register your models here.
 class leakdataAdmin(admin.ModelAdmin):
@@ -18,9 +19,14 @@ class emailListAdmin(admin.ModelAdmin):
     ]
     display_list = ('email')
 
-class ContactForm(admin.ModelAdmin):
-    # Contact Form for data to be submitted. 
-    pass
-    
+class contactformAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name',        {'fields': ['name']}),
+        ('Email',       {'fields': ['email']}),
+        ('description', {'fields': ['description']})
+    ]
+    displ_list = ('Name', 'Email', 'description')
+
 admin.site.register(leakdata, leakdataAdmin)
 admin.site.register(emailList, emailListAdmin)
+admin.site.register(contactForm, contactformAdmin)
