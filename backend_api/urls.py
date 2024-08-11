20752c9c4750ff621831b4f9652c38779ec9836d
leetjesus <JesusSecc@protonmail.com>
2024-08-10 18:41:56 -0700

    urls changed to handle api endpoints from app instead of main dir

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+# api/<fields>
+# "Ohhhh that is sick!" left off here
 urlpatterns = [
-    path('', views.search_email),
-    path('', views.home_page_view)
+    path('api/', views.search_email),
+    path('api/contact/', views.home_page_view)
 ]
