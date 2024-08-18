01502f4c82a582cf97d80d19292fec8a9bb0d523
leetjesus <JesusSecc@protonmail.com>
2024-08-18 15:49:29 -0700

    changed end point locations

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-# api/<fields>
-# "Ohhhh that is sick!" left off here
 urlpatterns = [
-    path('api/', views.search_email),
-    path('api/contact/', views.contact_form)
+    path('api/contact/', views.contact_form),
+    path('api/search/', views.search_email)
 ]
