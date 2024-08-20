70017db689943929cec2970a793749bae2a2ce33
leetjesus <JesusSecc@protonmail.com>
2024-08-19 18:33:38 -0700

    added extra result endpoint

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/contact/', views.contact_form),
-    path('api/email/<str:name>', views.email_search)
+    path('api/email/<str:name>', views.email_search),
+    path('api/result/<str:name>', views.email_result),
 ]
