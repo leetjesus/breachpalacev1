41815de84f67dcb361a3caeaf1198fbcaa0a5519
leetjesus <JesusSecc@protonmail.com>
2024-08-28 18:09:03 -0700

    URLS changed to accept class based EmailSearch

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import InfoBreach, EmailSearch, email_result
 
 urlpatterns = [
     path('api/contact/', views.contact_form),
-    path('api/email/<str:name>', views.email_search),
-    path('api/result/<str:name>', views.email_result),
+    path('api/email/<str:name>', views.EmailSearch.as_view(), name='email'),
+    path('api/result/', email_result),
+    # Original views before
+    # path('api/result/<str:name>', views.email_result)
 ]
