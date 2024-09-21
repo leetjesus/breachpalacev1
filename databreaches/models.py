812611aeb5fa1ba58944741e8dfc85bf8c49b9ac
leetjesus <JesusSecc@protonmail.com>
2024-09-21 14:34:36 -0700

    breach models removed

--- databreaches/models.py
+++ databreaches/models.py
@@ -2,16 +2,3 @@ from django.db import models
 from backend_api.models import breachInfo, emailList
 
 # Create your models here.
-class breachExample(models.Model):
-    # This would take the example of data breach (Myspace database dump)
-    email_id = models.ForeignKey(emailList, on_delete=models.CASCADE)
-    email = models.EmailField()
-    password = models.TextField()
-    breach_id = models.ForeignKey(breachInfo, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'breachExample'
-        verbose_name_plural = 'breachExample'
-    
-    def __str__(self):
-        return str(self.email_id)
