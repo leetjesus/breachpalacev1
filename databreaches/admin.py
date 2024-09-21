fead54d1356ae194133fc2b844e00e35bfc5378f
leetjesus <JesusSecc@protonmail.com>
2024-09-21 14:33:29 -0700

    admin register removed

--- databreaches/admin.py
+++ databreaches/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import breachExample
 
 # Register your models here.
-@admin.register(breachExample)
-class breachExampleAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('breach_id',        {'fields': ['email_id']}),
-        ('Name',        {'fields': ['email']}),
-        ('Description', {'fields': ['password']}),
-        ('Breach Date', {'fields': ['breach_id']})
-    ]
